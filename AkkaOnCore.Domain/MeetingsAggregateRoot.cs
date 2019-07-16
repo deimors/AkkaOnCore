4ddb01eb71478f52eaa9172db91fddcc69388ea9
deimors <deimors@gmail.com>
2019-07-15 18:10:56 -0600

    Added null / whitespace check to StartMeeting command

--- AkkaOnCore.Domain/MeetingsAggregateRoot.cs
+++ AkkaOnCore.Domain/MeetingsAggregateRoot.cs
@@ -13,17 +13,32 @@ namespace AkkaOnCore.Domain
 		public Result<IEnumerable<MeetingsEvent>, MeetingsCommandError> HandleCommand(MeetingsCommand command)
 			=> command.Match(StartMeeting);
 
-		private Result<IEnumerable<MeetingsEvent>, MeetingsCommandError> StartMeeting(MeetingsCommand.StartMeeting command)
-			=> Result.Create(
-				!_meetings.Values.Contains(command.Name), 
-				() => new MeetingsEvent[] { new MeetingsEvent.MeetingStartedEvent(Guid.NewGuid(), command.Name) }.AsEnumerable(),
-				() => new MeetingsCommandError($"Meeting with name '{command.Name}' already exists")
-			);
-
+		private Result<IEnumerable<MeetingsEvent>, MeetingsCommandError> StartMeeting(
+			MeetingsCommand.StartMeeting command)
+			=> Result.Unit<MeetingsCommandError>()
+				.FailIf(
+					_meetings.Values.Contains(command.Name),
+					() => new MeetingsCommandError($"Meeting with name '{command.Name}' already exists")
+				).FailIf(
+					string.IsNullOrWhiteSpace(command.Name),
+					() => new MeetingsCommandError("Meeting Name cannot be Empty or Whitespace")
+				).Select(_ => new MeetingsEvent[]
+					{
+						new MeetingsEvent.MeetingStartedEvent(Guid.NewGuid(), command.Name)
+					}
+					.AsEnumerable()
+				);
+				
 		public void ApplyEvent(MeetingsEvent meetingsEvent)
 			=> meetingsEvent.Apply(OnMeetingStarted);
 
 		private void OnMeetingStarted(MeetingsEvent.MeetingStartedEvent e)
 			=> _meetings.Add(e.MeetingId, e.Name);
+	}
+
+	public static class ResultExtensions
+	{
+		public static Result<TSuccess, TFailure> FailIf<TSuccess, TFailure>(this Result<TSuccess, TFailure> source, bool condition, Func<TFailure> failureFactory)
+			=> source.Bind(success => Result.Create(!condition, success, failureFactory()));
 	}
 }
