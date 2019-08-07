329f5c3c008cdf667bd7a171857e404aa4a04311
deimors <deimors@gmail.com>
2019-08-06 20:38:40 -0600

    React to agenda items added

--- AkkaOnCore.ReadHub/IMeetings.cs
+++ AkkaOnCore.ReadHub/IMeetings.cs
@@ -6,5 +6,6 @@ namespace AkkaOnCore.ReadHub
 	public interface IMeetings
 	{
 		Task OnMeetingAddedToList(string meetingId, string name);
+		Task OnMeetingAgendaCountChanged(string meetingId, int newCount);
 	}
 }
--- AkkaOnCore.ReadHub/MeetingsEventReader.cs
+++ AkkaOnCore.ReadHub/MeetingsEventReader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,40 +14,116 @@ using Newtonsoft.Json;
 
 namespace AkkaOnCore.ReadHub
 {
+	public delegate Task<IEnumerable<ResolvedEvent>> GetEventsSlice(string persistenceId, long startIndex, int count);
+
+	public delegate Task HandleEvent<TEvent>(TEvent @event);
+
+	public interface IProcessEvents
+	{
+		Task Process();
+	}
+
+	public class EventProcessor<TEvent> : IProcessEvents
+	{
+		private const int ChunkCount = 128;
+
+		private readonly string _persistenceId;
+		private readonly GetEventsSlice _getEventsSlice;
+		private readonly HandleEvent<TEvent> _handleEvent;
+
+		private long _nextEventIndex;
+
+		public EventProcessor(string persistenceId, GetEventsSlice getEventsSlice, HandleEvent<TEvent> handleEvent)
+		{
+			_persistenceId = persistenceId ?? throw new ArgumentNullException(nameof(persistenceId));
+			_getEventsSlice = getEventsSlice ?? throw new ArgumentNullException(nameof(getEventsSlice));
+			_handleEvent = handleEvent ?? throw new ArgumentNullException(nameof(handleEvent));
+		}
+
+		public async Task Process()
+		{
+			var slice = await _getEventsSlice(_persistenceId, _nextEventIndex, ChunkCount);
+
+			_nextEventIndex = await ProcessEvents(slice);
+		}
+
+		private async Task<long> ProcessEvents(IEnumerable<ResolvedEvent> events)
+		{
+			var nextEventIndex = _nextEventIndex;
+
+			foreach (var resolvedEvent in events)
+			{
+				Console.WriteLine($"Received {resolvedEvent.Event.EventType} ({resolvedEvent.Event.EventId})");
+
+				var @event = DeserializeEvent(resolvedEvent);
+
+				await _handleEvent(@event);
+
+				nextEventIndex = resolvedEvent.Event.EventNumber + 1;
+			}
+
+			return nextEventIndex;
+		}
+
+		private static TEvent DeserializeEvent(ResolvedEvent resolvedEvent)
+		{
+			dynamic metadata;
+
+			using (var stream = new MemoryStream(resolvedEvent.Event.Metadata))
+			using (var reader = new StreamReader(stream, Encoding.UTF8))
+			using (var jsonReader = new JsonTextReader(reader))
+			{
+				metadata = JsonSerializer.Create().Deserialize(jsonReader);
+			}
+
+			using (var stream = new MemoryStream(resolvedEvent.Event.Data))
+			using (var reader = new StreamReader(stream, Encoding.UTF8))
+			{
+				var eventType = Type.GetType((string)metadata.clrEventType);
+				var deserialized = JsonSerializer.Create().Deserialize(reader, eventType);
+				return (TEvent)deserialized;
+			}
+		}
+	}
 	public class MeetingsEventReader : BackgroundService
 	{
 		private readonly IHubContext<MeetingsHub, IMeetings> _meetingsHub;
 		private readonly ILogger<MeetingsEventReader> _logger;
 
-		private long _nextStartIndex;
 		private bool _isConnected;
+		private readonly IEventStoreConnection _connection;
+
+		private readonly List<IProcessEvents> _eventProcessors = new List<IProcessEvents>();
+
+		private readonly Dictionary<Guid, int> _agendaCounts = new Dictionary<Guid, int>();
 
 		public MeetingsEventReader(IHubContext<MeetingsHub, IMeetings> meetingsHub, ILogger<MeetingsEventReader> logger)
 		{
 			_meetingsHub = meetingsHub;
 			_logger = logger;
+			_connection = EventStoreConnection.Create("ConnectTo=tcp://admin:changeit@eventstore-node:1113; MaxReconnections=-1");
 		}
 
 		protected override async Task ExecuteAsync(CancellationToken cancelToken)
 		{
 			try
 			{
-				var connection = EventStoreConnection.Create("ConnectTo=tcp://admin:changeit@eventstore-node:1113; MaxReconnections=-1");
+				_connection.Connected += OnConnected;
+				_connection.Disconnected += OnDisconnected;
+				_connection.Closed += OnClosed;
+				_connection.ErrorOccurred += OnErrorOccurred;
 
-				connection.Connected += OnConnected;
-				connection.Disconnected += OnDisconnected;
-				connection.Closed += OnClosed;
-				connection.ErrorOccurred += OnErrorOccurred;
-
-				await connection.ConnectAsync();
+				await _connection.ConnectAsync();
 
 				_logger.LogDebug("After ConnectAsync()");
 
+				_eventProcessors.Add(new EventProcessor<MeetingsEvent>("MeetingsActor", ReadEvents, HandleMeetingsEvent));
+
 				while (!cancelToken.IsCancellationRequested)
 				{
-					if (_isConnected)
+					foreach (var processor in _eventProcessors.ToArray())
 					{
-						await ProcessEvents(connection);
+						await processor.Process();
 					}
 
 					await Task.Delay(TimeSpan.FromMilliseconds(10), cancelToken);
@@ -58,25 +136,26 @@ namespace AkkaOnCore.ReadHub
 			}
 		}
 
-		private async Task ProcessEvents(IEventStoreConnection connection)
-		{
-			var slice = await connection.ReadStreamEventsForwardAsync("MeetingsActor", _nextStartIndex, 128, false);
+		private async Task<IEnumerable<ResolvedEvent>> ReadEvents(string id, long start, int count)
+			=> _isConnected 
+				? (await _connection.ReadStreamEventsForwardAsync(id, start, count, false)).Events.AsEnumerable()
+				: Enumerable.Empty<ResolvedEvent>();
 
-			_nextStartIndex = slice.LastEventNumber + 1;
+		private Task HandleMeetingsEvent(MeetingsEvent meetingsEvent)
+			=> meetingsEvent.Match(
+				meetingStarted =>
+				{
+					_eventProcessors.Add(new EventProcessor<MeetingEvent>($"Meeting-{meetingStarted.MeetingId}", ReadEvents, HandleMeetingEvent));
+					return _meetingsHub.Clients.All.OnMeetingAddedToList(meetingStarted.MeetingId.ToString(), meetingStarted.Name);
+				});
 
-			foreach (var resolvedEvent in slice.Events)
-			{
-				_logger.LogInformation($"Received {resolvedEvent.Event.EventType} ({resolvedEvent.Event.EventId})");
+		private Task HandleMeetingEvent(MeetingEvent @event)
+			=> @event.Match(
+				itemAddedToAgenda => _meetingsHub.Clients.All.OnMeetingAgendaCountChanged(itemAddedToAgenda.MeetingId.ToString(), IncrementAgendaCount(itemAddedToAgenda.MeetingId))
+			);
 
-				var meetingsEvent = DeserializeEvent(resolvedEvent);
-
-				await meetingsEvent.Match(
-					meetingStarted
-						=> _meetingsHub.Clients.All.OnMeetingAddedToList(meetingStarted.MeetingId.ToString(),
-							meetingStarted.Name)
-				);
-			}
-		}
+		private int IncrementAgendaCount(Guid meetingId)
+			=> _agendaCounts[meetingId] = _agendaCounts.ContainsKey(meetingId) ? _agendaCounts[meetingId] + 1 : 1;
 
 		private void OnErrorOccurred(object sender, ClientErrorEventArgs e)
 		{
@@ -98,26 +177,6 @@ namespace AkkaOnCore.ReadHub
 		{
 			_logger.LogInformation("Connected");
 			_isConnected = true;
-		}
-
-		private static MeetingsEvent DeserializeEvent(ResolvedEvent resolvedEvent)
-		{
-			dynamic metadata;
-
-			using (var stream = new MemoryStream(resolvedEvent.Event.Metadata))
-			using (var reader = new StreamReader(stream, Encoding.UTF8))
-			using (var jsonReader = new JsonTextReader(reader))
-			{
-				metadata = JsonSerializer.Create().Deserialize(jsonReader);
-			}
-
-			using (var stream = new MemoryStream(resolvedEvent.Event.Data))
-			using (var reader = new StreamReader(stream, Encoding.UTF8))
-			{
-				var eventType = Type.GetType((string)metadata.clrEventType);
-				var deserialized = JsonSerializer.Create().Deserialize((TextReader) reader, eventType);
-				return (MeetingsEvent) deserialized;
-			}
 		}
 	}
 }
