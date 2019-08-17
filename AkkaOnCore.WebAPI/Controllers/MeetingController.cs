f8d0c3f6f620edd171c49ea095d2c73d0e4162a0
deimors <deimors@gmail.com>
2019-08-17 15:45:15 -0600

    Meeting View

---
+++ AkkaOnCore.WebAPI/Controllers/MeetingController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkkaOnCore.WebAPI.Controllers
+{
+	public class MeetingController : Controller
+	{
+		public IActionResult Index(Guid id)
+		{
+			ViewBag.MeetingId = id;
+
+			return View();
+		}
+	}
+}
